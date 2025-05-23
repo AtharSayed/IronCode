f90651abe1ce4aea1f1d61ae30781d47a383ca8b
Athar Sayed <sayedathar242@gmail.com>
2025-05-23 08:08:21 +0530

    Removed unused import

--- practice/prac1.py
+++ practice/prac1.py
@@ -1,8 +1,5 @@
 # Day-1 -21/5/2025
 # Write a function is_even(n) that takes an integer n and returns even and odd based on the number
-from multiprocessing.managers import Value
-
-
 def is_even(n):
     if n%2 ==0:
         return "Even"
