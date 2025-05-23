4f6a7540ca817387fe253534f1bdca2cd1c2d75d
Athar Sayed <sayedathar242@gmail.com>
2025-05-23 18:56:55 +0530

    Added more endpoints to test requests

--- practice/prac4.py
+++ practice/prac4.py
@@ -3,9 +3,21 @@ from flask import Flask,jsonify,request
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def index():
+    return jsonify({
+        "message": "Welcome to the Flask Math API",
+        "endpoints": {
+            "/sum (POST)": "Returns the sum of 'a' and 'b'",
+            "/max (POST)": "Returns the maximum of 'a' and 'b'",
+            "/testing (GET)": "Simple test message"
+        }
+    })
+
 @app.route('/sum',methods=['POST'])
 
-def sum():
+def max():
     data = request.get_json()
     a = data.get('a',0)
     b = data.get('b',0)
@@ -17,6 +29,7 @@ def testing():
     error = None
     if request.method == 'GET':
         return "Testing flask application !"
+
 
 if __name__ =="__main__":
     app.run(debug=True)
