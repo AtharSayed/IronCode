fd868bab64d27e33357087002431f675c8594ef5
Athar Sayed <sayedathar242@gmail.com>
2025-05-24 20:39:54 +0530

    Added negative values exception handling

--- practice/prac5.py
+++ practice/prac5.py
@@ -1,20 +1,23 @@
-# Python program to check palindrome
+# Python program to check for a palindrome
 
-def pal(n):
+
+def palindrome(n):
     rev = 0
-    temp = n
+    temp =n
     while temp > 0:
         digit = temp%10
-        rev = rev*10+digit
+        rev = rev*10 +digit
         temp = temp//10
     return rev
 
 try:
-    n = int(input("Enter the number : "))
-    res = pal(n)
-    if n == res:
-        print("The number is palindrome !")
-    else :
-        print("The number is not a palindrome! ")
+    n= int(input("Enter the number : "))
+    if n<0:
+        print("Enter a positive number !")
+    res = palindrome(n)
+    if n ==res:
+        print("The number is a palindrome !")
+    else:
+        print("The number is not a palindrome !")
 except ValueError:
-    print("Enter a valid number !")
+        print("Enter a valid number and try again !")
