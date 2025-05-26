16193619d6400c24b6863aece5c7cca640634a41
Athar Sayed <sayedathar242@gmail.com>
2025-05-26 22:32:04 +0530

    Using functions from different py file for reuseability

---
+++ practice/prac6.2.py
@@ -0,0 +1,12 @@
+from prac6 import checkarm  # Importing function from the previous code file
+
+if __name__=="__main__":
+    try:
+        n = int(input("Enter the number : "))
+        res = checkarm(n)
+        if res == n :
+            print("The number is armstrong number ")
+        else:
+            print("The number is not a armstrong number")
+    except ValueError:
+        print("Enter a valid number and try again !")
