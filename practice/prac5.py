45437abf48d488c26b98e3f3dc69e77c23ec8616
Athar Sayed <sayedathar242@gmail.com>
2025-05-28 11:46:59 +0530

    Implemented class and created method to check for palindrome

--- practice/prac5.py
+++ practice/prac5.py
@@ -1,25 +1,20 @@
 # Python program to check for a palindrome
+class Solution(object):
+    def isPalindrome(self, x):
+        if x < 0:
+            return False
+        rev = 0
+        temp = x
+        while temp > 0 :
+            digit = temp%10
+            rev = rev*10+digit
+            temp = temp //10
+        return rev ==x
 
-
-def palindrome(n):
-    rev = 0
-    temp =n
-    while temp > 0:
-        digit = temp%10
-        rev = rev*10 +digit
-        temp = temp//10
-    return rev
-
-# Adding main function as the main entry point for the program
-if __name__ =="__main__":
-    try:
-        n = int(input("Enter the number : "))
-        if n < 0:
-            print("Enter a positive number !")
-        res = palindrome(n)
-        if n == res:
-            print("The number is a palindrome !")
-        else:
-            print("The number is not a palindrome !")
-    except ValueError:
-        print("Enter a valid number and try again !")
+if __name__=="__main__":
+    num = Solution()
+    x = 121
+    if num.isPalindrome(x):
+        print("The number is Palindrome")
+    else:
+        print("The number is not palindrome")
