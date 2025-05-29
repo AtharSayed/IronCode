7565a255d0e91bc9a81b52a2b1bc92c44ebed86b
Athar Sayed <sayedathar242@gmail.com>
2025-05-29 10:40:20 +0530

    Added Exception Handling

--- practice/prac8.py
+++ practice/prac8.py
@@ -1,21 +1,24 @@
-## Roman to Int using class and functions
-class Solution(object):
+## Roman to Integer
+
+class Solution():
     def romantoInt(self,s):
         total = 0
-        prev_value = 0
-        # Creating Dictionary for roman numbers and corresponding integer
-        romans_int = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+        prev_state = 0
+        romans_map = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         for ch in reversed(s):
-            current = romans_int[ch]
-            if current < prev_value:
-                total -=current
+            current = romans_map[ch]
+            if current < prev_state:
+                total -= current
             else:
                 total +=current
-                prev_value=current
+                prev_state=current
         return total
 
 if __name__=="__main__":
-    num1 = Solution()
-    x = "XVII"
-    res = num1.romantoInt(x)
-    print("The number is :",res)
+    demo = Solution()
+    try:
+        x = str(input("Enter the roman numeral : "))
+        res = demo.romantoInt(x)
+        print("The integer representation of the number is : ", res)
+    except ValueError as e :
+        print("Error",e)
