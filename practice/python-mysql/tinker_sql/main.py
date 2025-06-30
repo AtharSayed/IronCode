8f1edb1c0a06d1ae16cb2c0e6fd298f4ebae85e8
Athar Sayed <sayedathar242@gmail.com>
2025-06-30 16:59:46 +0530

    Removed Duplicate entries from the table of a specific entry

--- practice/python-mysql/tinker_sql/main.py
+++ practice/python-mysql/tinker_sql/main.py
@@ -10,7 +10,21 @@ mycursor = mydb.cursor("CREATE DATABASE if not exists testdatabase")
 mycursor = mydb.cursor("USE testdatabase")
 # mycursor.execute("CREATE TABLE Person(name VARCHAR(50), age smallint UNSIGNED, PersonID int PRIMARY KEY AUTO_INCREMENT)")
 
-mycursor.execute("DESCRIBE Person")
+# Inserting values in the database 
+
+# mycursor.execute(" INSERT INTO Person (name,age) VALUES (%s,%s)",("Tim",19))
+# mycursor.execute(" INSERT INTO Person (name,age) VALUES (%s,%s)",("Joe",23))
+# mycursor.execute(" INSERT INTO Person (name,age) VALUES (%s,%s)",("Rae",29))
+
+# Commiting the values after inserting for permanent change in the database 
+mydb.commit()
+
+# Deleting the duplicate values from the table 
+mycursor.execute("DELETE FROM Person WHERE name = 'Tim' ")
+mydb.commit()
+mycursor.execute("SELECT *FROM Person")
+
+# mycursor.execute("DESCRIBE Person")
 for x in mycursor:
     print(x)
     
