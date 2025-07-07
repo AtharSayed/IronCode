f344a50bfc15fdc86b5654a0dd75437a56359527
Athar Sayed <99021665+AtharSayed@users.noreply.github.com>
2025-07-07 10:49:50 +0530

    Fixed indentation

--- practice/core-II DSA/Selectionsort.py
+++ practice/core-II DSA/Selectionsort.py
@@ -1,13 +1,12 @@
 # Implementing Selection Sort
-
 def selectionsort(mylist):
     n = len(mylist)
-    for i in range(n):
+    for i in range(n-1):
         min_index = i
         for j in range(i+1,n):
             if mylist[j]<mylist[min_index]:
-                min_index=j
-            mylist[i],mylist[min_index]=mylist[min_index],mylist[i]
+                min_index = j
+        mylist[i],mylist[min_index]=mylist[min_index],mylist[i]
     return mylist
     
 if __name__ =="__main__":
