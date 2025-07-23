c870d808664b2e0773e7042a584cb181a4b7562f
Athar Sayed <99021665+AtharSayed@users.noreply.github.com>
2025-07-23 19:11:02 +0530

    Update Advanced_Search.py

--- practice/core-II DSA/Advanced_Search.py
+++ practice/core-II DSA/Advanced_Search.py
@@ -1,35 +1,28 @@
-# Implementing Binary Search by sorting unsorted array with bubble sort first and then finding the target element 
-
-def sorting(nums):
-    swap = True
-    while swap:
-        swap = False
-        i = 0
-        while i <len(nums)-1:
-            if nums[i]>nums[i+1]:
-                nums[i],nums[i+1]=nums[i+1],nums[i]
-                swap =True
-                i+=1
-            else:
-                i+=1
-    return nums
-def search(nums,target):
-    n = len(nums)
+# Sorting unsorted array and then searching using binary Search
+def search(nums, target):
     left = 0
-    right = len(nums)-1
-    while left<=right:
-        mid = (left+right)//2
-        if nums[mid]==target:
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
             return mid
         elif nums[mid] < target:
-            left = mid+1
+            left = mid + 1
         else:
-            right = mid-1
+            right = mid - 1
     return -1
 
-if __name__ =="__main__":
-    nums = [4,3,1,7,9,2,8]
-    sorting(nums)
-    target =int(input("Enter the number : "))
-    res = search(nums,target)
-    print(res)
+def bubble(nums):
+    n = len(nums)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+    return nums
+
+if __name__ == "__main__":
+    nums = list(map(int, input("Enter the list of numbers: ").split()))
+    target = int(input("Enter the target number: "))
+    bubble(nums)
+    res = search(nums, target)
+    print("Index of target (in sorted array):", res)
